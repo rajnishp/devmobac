edb5895f169c129a1ea5912c224f3a5363b44cdd
rajnishp <rajnish_pawar90@yahoo.com>
2015-03-21 04:54:27 +0530

    user info set

--- mobac/backend/src/models/UserInfo.class.php
+++ mobac/backend/src/models/UserInfo.class.php
@@ -2,17 +2,95 @@
 	/**
 	 * Object represents table 'user_info'
 	 *
-     	 * @author: http://phpdao.com
+     	 * @author: rajnish
      	 * @date: 2015-03-18 23:07	 
 	 */
 	class UserInfo{
 		
-		var $id;
-		var $firstName;
-		var $lastName;
-		var $email;
-		var $phoneNo;
-		var $password;
-		
+		private $id;
+		private $firstName;
+		private $lastName;
+		private $email;
+		private $phoneNo;
+		private $password;
+		private $status;
+		
+
+		function __construct($firstName, $lastName, $email, $phoneNo, $password, $status, $id = null) {
+			$this-> id = $id;
+			$this-> firstName = $firstName;
+			$this-> lastName = $lastName;
+			$this-> email = $email;
+			$this-> phoneNo = $phoneNo;
+			$this-> password = $password;
+			$this-> status = $status;
+
+			
+		}
+		
+		function setId($id){
+			$this -> id = $id;
+		}
+		function getId(){
+				return $this-> id;
+		}
+
+		function setFirstName($firstName){
+			$this -> firstName = $firstName;
+		}
+		function getFirstName(){
+				return $this-> firstName;
+		}
+
+		function setLastName($lastName){
+			$this -> lastName = $lastName;
+		}
+		function getLastName(){
+				return $this-> lastName;
+		}
+
+		function setEmail($email){
+			$this -> email = $email;
+		}
+		function getEmail(){
+				return $this-> email;
+		}
+
+		function setPhoneNo($phoneNo){
+			$this -> phoneNo = $phoneNo;
+		}
+		function getPhoneNo(){
+				return $this-> phoneNo;
+		}
+
+		function setPassword($password){
+			$this -> password = $password;
+		}
+		function getPassword(){
+				return $this-> password;
+		}
+
+		function setStatus($status){
+			$this -> status = $status;
+		}
+		function getStatus(){
+				return $this-> status;
+		}
+
+		function toString (){
+			return $this -> id . ", " . $this -> firstName . ", " . $this -> lastName. ", " . $this -> email. ", " . $this -> phoneNo. ", " . $this -> password . ", " . $this -> status;
+		}
+		
+		function toArray() {
+			return array (
+						id => $this-> id,
+						userId => $this-> firstName,
+						fromTo => $this-> lastName,
+						messageText => $this-> email,
+						time => $this-> phoneNo,
+						type => $this-> password,
+						status => $this-> status
+				);
+		}
 	}
 ?>
