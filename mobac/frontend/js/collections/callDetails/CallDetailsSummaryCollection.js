b41821dde5cda6d81b7b0b39884bf19c15e19dda
anil-ninja <kumar.anil8892@gmail.com>
2015-04-01 19:37:51 +0530

    ui changed

---
+++ mobac/frontend/js/collections/callDetails/CallDetailsSummaryCollection.js
@@ -0,0 +1,24 @@
+define([
+  'underscore',
+  'backbone',
+  'models/callDetails/CallDetailsModel'
+], function(_, Backbone, CallDetailsModel){
+
+  var CallDetailsCollection = Backbone.Collection.extend({
+      
+      model: CallDetailsModel,
+
+      initialize : function(models, options) {
+        
+        console.log("starting CallDetails");
+      },
+      
+      url : function() {
+        return window.BASE_URL+'/callDetails-summary';
+      }        
+     
+  });
+
+  return CallDetailsCollection;
+
+});
